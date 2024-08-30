88d05b76aa3202732c056f0f67bb6b8ab221fcda
quarkum-0 <122341624+quarkum-0@users.noreply.github.com>
2024-08-30 08:17:10 +0530

    should work

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -28,6 +28,7 @@ import News from "../../assets/home/news";
 import Media from "react-media";
 import Navbar from "../../components/NavbarNew/Navbar";
 import LOGO from '../../assets/home/hero/logo.jpg'
+import BarWave1 from "react-cssfx-loading/lib/FadingBalls";
 import { useHistory } from "react-router-dom";
 
 function Home() {
@@ -348,7 +349,7 @@ function Home() {
       </div>
       ) : 
       (<div style={{height:"100vh", width:"100%", background:"black", position:"absolute"}}>
-        <div style={{marginTop:"50vh", marginLeft:"47vw"}}></div>
+        <div style={{marginTop:"50vh", marginLeft:"47vw"}}><BarWave1 color="red" height="25px" width="25px" /></div>
 
       </div>)
         }
--- src/pages/clubs/clubs.js
+++ src/pages/clubs/clubs.js
@@ -6,6 +6,7 @@ import Testevents from './testevents'
 import ClubPOR from "./ClubPOR";
 import Activities from "./activities";
 import Clubvideo from "./clubvideo";
+import BarWave from "react-cssfx-loading/lib/FillingBottle";
 import { getDocData, getData } from "../../functions/firestore";
 import footer from '../../assets/images/footer.png'
 
@@ -49,7 +50,8 @@ function Clubs() {
       ) : (<>
         <div
           style={{marginTop:"35vh", marginLeft:"47vw"}}
-        ></div>
+        >
+        <BarWave color="#FF0000" width="40px" height="40px" /></div>
       </>)}
 
       <Footer background={footer}/>
